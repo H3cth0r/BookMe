15f304575fbd4cc635a6da4726bc42ec994c093a
h3cth0r <hectorsucre13@gmail.com>
2022-09-29 14:15:50 -0500

    validating account config formulary functionality running

--- BookMe/AccountConfigViewController.swift
+++ BookMe/AccountConfigViewController.swift
@@ -11,6 +11,17 @@ class AccountConfigViewController: UIViewController {
 
     @IBOutlet var vwContainer: UIView!
     
+    // formulary input field label
+    @IBOutlet weak var nameTextInputLabel: UILabel!
+    @IBOutlet weak var surnameTextInputLabel: UILabel!
+    @IBOutlet weak var usernameTextInputLabel: UILabel!
+    @IBOutlet weak var birthTextInputLabel: UILabel!
+    @IBOutlet weak var organizationTextInputLabel: UILabel!
+    @IBOutlet weak var labelTextInputLabel: UILabel!
+    @IBOutlet weak var passwordOneTextInputLabel: UILabel!
+    @IBOutlet weak var passwordTwoTextInputLabel: UILabel!
+    
+    
     // forumlary input fields
     @IBOutlet weak var nameTextInput: UITextField!
     @IBOutlet weak var surnameTextInput: UITextField!
@@ -20,6 +31,16 @@ class AccountConfigViewController: UIViewController {
     @IBOutlet weak var mailTextInput: UITextField!
     @IBOutlet weak var passwordOneTextInput: UITextField!
     @IBOutlet weak var passwordTwoTextInput: UITextField!
+    
+    // Old textfield values
+    var oldNameText: String!
+    var oldSurnameText: String!
+    var oldUsernameText: String!
+    var oldBirthText: String!
+    var oldOrganizationText: String!
+    var oldMailText: String!
+    var oldPasswordOneText: String!
+    var oldPassqoesTwoText: String!
     
     
     @IBOutlet weak var confirmChangesView: UIView!
@@ -59,7 +80,11 @@ class AccountConfigViewController: UIViewController {
         
         // Do any additional setup after loading the view.
         confirmChangesView.isHidden = true
+        
+        // Saving starting values to oldvalues
+        setOldValues()
     }
+    
     // date picker functionality
     @objc func dateChange(datePicker: UIDatePicker){
         let dat = formatDate(date: datePicker.date)
@@ -98,7 +123,10 @@ class AccountConfigViewController: UIViewController {
     @IBAction func openConfirmationButton(_ sender: Any) {
         self.view.endEditing(true)
         editingSomeTextInput = false
-        confirmChangesView.isHidden = false
+        if(checkDataChanges() && thereAreChanges()){
+            confirmChangesView.isHidden = false
+        }
+        
     }
     @IBAction func saveDataButton(_ sender: Any) {
         self.view.endEditing(true)
@@ -118,6 +146,130 @@ class AccountConfigViewController: UIViewController {
         confirmChangesView.isHidden = true
     }
     
+    func setOldValues(){
+        oldNameText         = nameTextInput.text
+        oldSurnameText      = surnameTextInput.text
+        oldUsernameText     = usernameTextInput.text
+        oldBirthText        = birthTextInput.text
+        oldOrganizationText = organizationTextInput.text
+        oldMailText         = mailinputspace.text
+        oldPasswordOneText  = passwordOneTextInput.text
+        oldPassqoesTwoText  = passwordTwoTextInput.text
+    }
+    
+    func thereAreChanges()->Bool{
+        if(nameTextInput.text != oldNameText || surnameTextInput.text != oldSurnameText || usernameTextInput.text != oldUsernameText || birthTextInput.text != oldBirthText || organizationTextInput.text != oldOrganizationText || mailinputspace.text != oldMailText || passwordOneTextInput.text != oldPasswordOneText || passwordTwoTextInput.text != oldPassqoesTwoText){
+            return true
+        }
+        return false
+    }
+    
+    // Check user input fields data
+    func checkDataChanges()->Bool{
+        
+        var allOkay = true
+        
+        // setting default color
+        nameTextInputLabel.textColor = .black
+        surnameTextInputLabel.textColor = .black
+        usernameTextInputLabel.textColor = .black
+        labelTextInputLabel.textColor = .black
+        passwordOneTextInputLabel.textColor = .black
+        passwordTwoTextInputLabel.textColor = .black
+        
+        if(oldNameText != nameTextInput.text){
+            let currentName = nameTextInput.text ?? "Pepo"
+            if !isValidName(currentName){
+                nameTextInputLabel.textColor = .red
+                allOkay = false
+            }else{
+                nameTextInputLabel.textColor = .black
+            }
+        }
+        
+        if(surnameTextInput.text != oldSurnameText){
+            let currentSurname = surnameTextInput.text ?? "Smith"
+            if !isValidName(currentSurname){
+                surnameTextInputLabel.textColor = .red
+                allOkay = false
+            }else{
+                surnameTextInputLabel.textColor = .black
+            }
+        }
+        
+        if(oldUsernameText != usernameTextInput.text){
+            // service to check if username exists
+            let currentUsername = usernameTextInput.text ?? "pepo117"
+            if !isValidUsername(currentUsername){
+                usernameTextInputLabel.textColor = .red
+                allOkay = false
+            }else{
+                usernameTextInputLabel.textColor = .black
+            }
+            print(usernameTextInput.text ?? "none")
+        }
+        
+        if(oldMailText != mailinputspace.text){
+            // check if valid mail
+            let currentMail = mailinputspace.text ?? "work@gmail.com"
+            if(!isValidEmail(currentMail)){
+                labelTextInputLabel.textColor = .red
+                allOkay = false
+            }else{
+                labelTextInputLabel.textColor = .black
+            }
+            
+        }
+        
+        if(oldPasswordOneText != passwordOneTextInput.text || oldPassqoesTwoText != passwordTwoTextInput.text){
+            //  check password structure
+            let currentPassword = passwordOneTextInput.text ?? "pepito22@smith"
+            if(!isValidPassword(password: currentPassword)){
+                passwordOneTextInputLabel.textColor = .red
+                passwordTwoTextInputLabel.textColor = .red
+                allOkay = false
+            }else{
+                passwordOneTextInputLabel.textColor = .black
+                passwordTwoTextInputLabel.textColor = .black
+            }
+            if(passwordOneTextInput.text != passwordTwoTextInput.text){
+                // just color the second, to indicate they are not the same
+                passwordTwoTextInputLabel.textColor = .red
+                allOkay = false
+            }else{
+                passwordTwoTextInputLabel.textColor = .black
+            }
+        }
+        
+        return allOkay
+        
+    }
+    
+    func isValidName(_ name: String) -> Bool {
+        //Declaring the rule of characters to be used. Applying rule to current state. Verifying the result.
+        let regex = "[A-Za-z]{2,}"
+        let test = NSPredicate(format: "SELF MATCHES %@", regex)
+        let result = test.evaluate(with: name)
+        
+        return result
+    }
+    func isValidUsername(_ username:String) -> Bool {
+        let RegEx = "\\w{7,18}"
+        let Test = NSPredicate(format:"SELF MATCHES %@", RegEx)
+        return Test.evaluate(with: username)
+    }
+    func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    // https://stackoverflow.com/questions/57771505/use-regex-to-validate-a-password-on-ios-swift
+    func isValidPassword(password: String) -> Bool {
+        let passRegEx = "(?=[^a-z]*[a-z])(?=[^0-9]*[0-9])[a-zA-Z0-9!@#$%^&*]{8,}"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passRegEx)
+        return passwordTest.evaluate(with: password)
+    }
     /*
     // MARK: - Navigation
 
